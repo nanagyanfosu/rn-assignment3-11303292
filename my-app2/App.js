2422bc3f02ad9570a860439dd193149dd9190877
nanagyanfosu <nanagyanfosu@gmail.com>
2024-06-01 17:41:32 +0000

    styled 'tasks' section

--- my-app2/App.js
+++ my-app2/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { StyleSheet, View, ScrollView, Text, SafeAreaView, Image, TextInput } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { Dimensions } from 'react-native';
 
 
 export default function App() {
   return (
-    <ScrollView style={styles.container}>
+    <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
       <View style={styles.row}>
         <View style={{ flexDirection: 'column' }}>
           <Text style={{ fontSize: 36 }}>Hello, Devs</Text>
           <Text>14 tasks today</Text>
         </View>
-        <Image source={require('./assets/person.png')} style={{ width: 40, height: 40, backgroundColor: 'black', borderRadius: 100 }} />
+        <Image source={require('./assets/person.png')} style={{ width: 40, height: 40, backgroundColor: 'white', borderRadius: 100 }} />
       </View>
 
       <View style={styles.searchRow}>
@@ -23,15 +24,19 @@ export default function App() {
 
       </View>
 
+
       <View style={styles.content}>
+
         <View>
           <Text style={{ fontSize: 28, paddingTop: 50 }}>Categories</Text>
         </View>
 
         <View style={styles.generalContent}>
+
           <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
 
             <View style={{ flexDirection: 'row', paddingTop: 15 }}>
+
               <View style={styles.card} backgroundColor="white">
                 <Text style={{ fontSize: 21 }}>Exercise</Text>
                 <Text>12 tasks</Text>
@@ -57,15 +62,38 @@ export default function App() {
               </View>
 
             </View>
+
           </ScrollView>
+
+        </View>
+
+        <View>
+          <Text style={{ fontSize: 28, paddingTop: 40 }}>Ongoing Task</Text>
+        </View>
+        <View style={{ flexDirection: 'column', paddingTop: 10 }}>
+
+          <View style={styles.task} backgroundColor='white'>
+            <Text style={{ fontSize: 20, }}>Mobile App Development</Text>
+          </View>
+
+          <View style={styles.task} >
+            <Text style={{ fontSize: 20, }}>Web Development</Text>
+          </View>
+
+          <View style={styles.task} backgroundColor='white'>
+            <Text style={{ fontSize: 20, }}>Push Ups</Text>
+          </View>
+
+
         </View>
 
       </View>
-
-
     </ScrollView>
   );
 };
+
+const screenWidth = Dimensions.get('window').width;
+
 
 const styles = StyleSheet.create({
   container: {
@@ -94,7 +122,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     borderRadius: 10,
     height: 45,
-    width: 270,
+    width: screenWidth - 100,
     padding: 5,
   },
 
@@ -114,7 +142,21 @@ const styles = StyleSheet.create({
     padding: 15,
     marginRight: 20,
 
-  }
+  },
 
-});
+  task: {
+    margin: 10,
+    height: 150,
+    width: screenWidth - 50,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 15,
+    alignItems: 'left',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+}
+
+);
 
